5da0c71ae1d9ebfa72f01d0cd431d3f7756c6cea
Padhraic <pcoogan@gmail.com>
2015-11-19 20:22:56 +0900

    add edit unit tests

--- TestProject/CodeFirst/TrackerEnabledDbContext/TrackerEnabledDbContextTest.cs
+++ TestProject/CodeFirst/TrackerEnabledDbContext/TrackerEnabledDbContextTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,7 +11,9 @@ namespace TestProject.CodeFirst.TrackerEnabledDbContext
     [TrackChanges]
     public class Transaction
     {
-        public int TransactionId { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public Money Money { get; set; }
         public string Detail { get; set; }
     }
@@ -64,7 +67,7 @@ namespace TestProject.CodeFirst.TrackerEnabledDbContext
             ctx.SaveChanges("TrackerEnabledDbContext_add");
             // Assert
             var count = ctx.Transactions.Count();
-            var savedTrx = ctx.Transactions.OrderByDescending(t => t.TransactionId).First();
+            var savedTrx = ctx.Transactions.OrderByDescending(t => t.Id).First();
             Assert.AreEqual(2, count);
             Assert.AreEqual(savedTrx.Money, trx.Money);
             Assert.AreEqual(savedTrx.Detail, trx.Detail);
@@ -103,5 +106,56 @@ namespace TestProject.CodeFirst.TrackerEnabledDbContext
             Assert.AreEqual(trx.Money.Amount, 123456789012.35m);
             Assert.AreEqual(trx.Money.CurrencyCode, "USD");
         }
+
+        [TestMethod]
+        public void EFExtendedCodeFirst_Edit()
+        {
+            // Arrange
+            var ctx = new TrackerEnabledDbContextEntities();
+            var trx = new Transaction
+            {
+                Money = 1.23d,
+                Detail = "Another Transaction To be edited"
+            };
+            ctx.Transactions.Add(trx);
+            ctx.SaveChanges();
+
+            //Act
+            trx.Money = 10d;
+            var t = ctx.Set<Transaction>().FirstOrDefault(x => x.Id == trx.Id);
+            ctx.Entry(t).CurrentValues.SetValues(trx);
+            ctx.Entry(t).State = EntityState.Modified;
+            ctx.SaveChanges("EFExtendedCodeFirst_Edit");
+
+            // Assert
+            Assert.IsTrue(t?.Money.InternalAmount == 10d, "assert the object was updated correctly");
+        }
+
+        [TestMethod]
+        public void EFExtendedCodeFirst_Edit_origional_different_from_current()
+        {
+            // Arrange
+            var ctx = new TrackerEnabledDbContextEntities();
+            var trx = new Transaction
+            {
+                Money = 1.23d,
+                Detail = "Another Transaction To be edited"
+            };
+            ctx.Transactions.Add(trx);
+            ctx.SaveChanges();
+
+            //Act
+            trx.Money = 10d;
+            var t = ctx.Set<Transaction>().FirstOrDefault(x => x.Id == trx.Id);
+            ctx.Entry(t).CurrentValues.SetValues(trx);
+            ctx.Entry(t).State = EntityState.Modified;
+            ctx.SaveChanges();
+            var log = ctx.GetLogs(typeof (Transaction).FullName, t.Id).Max(tran => tran.AuditLogId);
+            var logDetail = ctx.LogDetails.FirstOrDefault(d => d.AuditLogId == log && d.PropertyName == "Money");
+
+            // Assert
+            Assert.AreNotEqual(logDetail?.OriginalValue, logDetail?.NewValue);
+        }
+
     }
 }
